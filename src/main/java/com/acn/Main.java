cf9b45a5d0e2c0f6b7763c584ebf50b1df022e08
zliimt717 <xin.gu1707@gmail.com>
2023-05-03 21:44:23 +0200

    multi language

---
+++ src/main/java/com/acn/Main.java
@@ -0,0 +1,34 @@
+package com.acn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+
+@SpringBootApplication
+public class Main {
+    //configuring default locale
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+    // configuring ResourceBundle
+    @Bean
+    public ResourceBundleMessageSource bundleMessageSource(){
+        ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+}
